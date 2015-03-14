77277be4760665fca70d912204de2c9d408fb75d
Eric Gonzalo <ersgonzalo@gmail.com>
2015-03-14 01:17:47 -0400

    Working music player basics, volume control should be next

--- readme.md
+++ readme.md
@@ -1,12 +1,18 @@
-Building this project during my Personal Hackathon time at Fullstack Academy. At the moment, is a website that will play some various local music. At the moment...not functional at all. At the end, it will try to replicate foobar2000 functionality!
+Building this project during my Personal Hackathon time at Fullstack Academy. At the moment, is a website that will play some various local music. At the moment...functional enough. At the end, it will hopefully replicate foobar2000 functionality!
 
 To Dos:
 ------------
+- Refactor all the ugly(Directives, Factories, etc)
 - Drag Files in to add Songs
+- Get Tag Data from Files
+- Allow Editing of Tags
+- FLAC Support
 - Playlists
+- Last.fm scrobbling
 - Build it as a Chrome App
 - Build as a Firefox Extension?
 - Save & Export Library Configuration
+- Draggable Elements?
 
 Possibly to Use:
 ------------
@@ -14,7 +20,11 @@ Possibly to Use:
 - [Browserify](https://github.com/substack/node-browserify)
 - [MusicMetaData](https://github.com/leetreveil/musicmetadata)
 - [Flac.js and Aurora](http://audiocogs.org/articles/2012/06/15/flac-and-aurora/)
+- [Range Style Generation](http://danielstern.ca/range.css/#/)
 
 Special thanks to:
 ------------
-- [Stack Overflow](http://stackoverflow.com/) *I'd wouldn't be where I am now with you guys!*
+- Chris for helping me get started from the very beginning.
+- [Stack Overflow](http://stackoverflow.com/)
+- [Google](https://github.com/google/material-design-icons) for their material design icons
+- [Bootstrap](http://getbootstrap.com/)
--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,65 @@
 var app = angular.module('OngakuPlay', []);
 
 app.controller('MainController', function($scope){
-	console.log("Loaded! :)");
+	// console.log("Loaded! :)");
 
 	var nowIndex = 0;
 	var music = document.getElementById('audiotrack');
+	var seekbar = document.getElementById('seekbar');
+	seekbar.value = 0;
+
+	music.addEventListener("ended", function(){
+		music.currentTime = 0;
+		setPlayButton();
+	});
+
+	music.ondurationchange = setUpSeekbar;
+	music.addEventListener("durationchange", setUpSeekbar);
+	music.addEventListener("timeupdate", updateUI);
+
+	var setUpSeekbar = function(){
+		seekbar.min = music.startTime;
+		seekbar.max = music.startTime + music.duration;
+	}
+	var seekAudio = function() {
+        music.currentTime = seekbar.value;
+    }
+	var updateUI = function(){
+		var lastBuffered = music.buffered.end(music.buffered.length-1);
+		seekbar.min = music.startTime;
+		seekbar.max = lastBuffered;
+		seekbar.value = music.currentTime;
+    }
+    seekbar.onchange = seekAudio;
+    music.ontimeupdate = updateUI;
+
+    var timeConverter = function(songDuration){
+    	if (songDuration < 60)
+    		return Math.ceil(songDuration + 1) + " seconds"
+    	else{
+    		var mins = parseInt(songDuration/60) + " minutes and ";
+    		var secs = parseInt(songDuration % 60) + " seconds";
+    		return mins + secs;
+    	}
+    }
+
+    var getSongTime = function(){
+    	var initTime = null;
+    	initTime = music.duration;
+    	$scope.songTime = timeConverter(initTime);
+    }
+
+	$scope.isPlaying = true;
+
+	var setPlayButton = function(){
+		$scope.isPlaying = true;
+		$scope.$digest();
+	}
 
 	$scope.songs = [
 		//test files
 		"/media/eric/82C48964C4895AF51/Users/Light/Music/Ambient_-Ortiiz-10445_hifi.mp3",
+		"/media/eric/82C48964C4895AF51/Users/Light/Music/Persona 4 Chime.mp3",
 		"/media/eric/82C48964C4895AF51/Users/Light/Music/04 - Everything Is Under Control (DJ Kentaro remix).mp3",
 		"/media/eric/82C48964C4895AF51/Users/Light/Music/The Strokes/[2003] Room On Fire/02 - Reptilia.mp3",
 		"/media/eric/82C48964C4895AF51/Users/Light/Music/日本の音楽/goose house/光るなら - EP/01 光るなら.m4a",
@@ -18,40 +69,33 @@ app.controller('MainController', function($scope){
 
 	$scope.currentSong = $scope.songs[nowIndex];
 
-	var timeConverter = function(songDuration){
-		if (songDuration < 60)
-			return Math.ceil(songDuration + 1) + " seconds"
-		else{
-			var mins = parseInt(songDuration/60) + " minutes and ";
-			var secs = parseInt(songDuration % 60) + " seconds";
-			return mins + secs;
-		}
-	}
 
-	var getSongTime = function(){
-		var initTime = null;
-		initTime = music.duration;
-		$scope.songTime = timeConverter(initTime);
-	}
-
-	$scope.playAudio = function() {
+	$scope.playTrack = function() {
 		getSongTime();
 		if (music.paused) {
-			$scope.isPlaying = true;
+			$scope.isPlaying = false;
 			music.play();
 		} else {
-			$scope.isPlaying = false;
+			$scope.isPlaying = true;
 			music.pause();
 		}
 	}
 
+	$scope.stopTrack = function(){
+		music.pause();
+		music.currentTime = 0;
+		$scope.isPlaying = true;
+	}
+
 	$scope.playThisSong = function(song){
 		$scope.currentSong = song;
+		seekbar.value = 0;
+		$scope.isPlaying = true;
 		getSongTime();
-		var parsers = mm(fs.createReadStream($scope.currentSong), function(err, metadata){
-			if(err) throw err;
-			$scope.songNowData = metadata;
-		});
+		// var parsers = mm(fs.createReadStream($scope.currentSong), function(err, metadata){
+		// 	if(err) throw err;
+		// 	$scope.songNowData = metadata;
+		// });
 	}
 
 	$scope.advanceTrack = function(){
@@ -64,5 +108,14 @@ app.controller('MainController', function($scope){
 		nowIndex--;
 	}
 
+	$scope.loopTrack = function(){
+		if(music.loop){
+			music.loop = false;
+			$('.fa-retweet').css('background-color: #673AB7');
+		}else{
+			music.loop = true;
+			$('.fa-retweet').css('background-color: #F44336');
+		}
+	}
 
 });
